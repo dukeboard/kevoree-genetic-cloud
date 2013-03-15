44d5a62c0b42a387c34f5fda6c9dad029e94da83
Grégory NAIN <gregory.nain@gmail.com>
2013-03-15 19:35:52 +0100

    Adding random generation

--- org.kevoree.genetic.cloud.reasoner/src/main/java/org/kevoree/genetic/cloud/reasoner/RunnerRandomSolution.java
+++ org.kevoree.genetic.cloud.reasoner/src/main/java/org/kevoree/genetic/cloud/reasoner/RunnerRandomSolution.java
@@ -1,49 +1,76 @@
 package org.kevoree.genetic.cloud.reasoner;
 
+import org.kevoree.ContainerRoot;
+import org.kevoree.genetic.KevoreeEngineInstrument;
 import org.kevoree.genetic.cloud.reasoner.fitness.*;
 import org.kevoree.genetic.cloud.reasoner.operators.AddVirtualNodeOperator;
 import org.kevoree.genetic.cloud.reasoner.operators.OptimizeRedondencyOperator;
 import org.kevoree.genetic.cloud.reasoner.operators.RandomAddComponentOperator;
+import org.kevoree.genetic.cloud.reasoner.plot.SolutionPloter;
 import org.kevoree.genetic.cloud.reasoner.population.CloudPopulationFactory;
+import org.kevoree.genetic.cloud.reasoner.population.RandomSolutionFactory;
+import org.kevoree.genetic.framework.KevoreeCompositeFitnessFunction;
+import org.kevoree.genetic.framework.KevoreeFitnessFunction;
 import org.kevoree.genetic.framework.KevoreeGeneticEngine;
 import org.kevoree.genetic.framework.KevoreeSolution;
 import org.kevoree.genetic.library.operator.RemoveChildNode;
 import org.kevoree.genetic.library.operator.RemoveComponent;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 public class RunnerRandomSolution {
 
     public static void main(String[] args) throws Exception {
-        KevoreeGeneticEngine engine = new KevoreeGeneticEngine()
-                .setPopulationFactory(new CloudPopulationFactory());
 
-        /* Configure operator */
-        RandomAddComponentOperator operator = new RandomAddComponentOperator();
-        operator.addType("ItemDB").addType("LoadBalancer").addType("PaymentDB").addType("UserDB").addType("WebFrontend");
-        operator.setSelectorQuery("nodes[{ typeDefinition.name = *CustomerNode }]");
-        engine.addOperator(operator);
-        engine.addOperator(new AddVirtualNodeOperator().setSelectorQuery("nodes[{ typeDefinition.name = *InfraNode }]"));
-        engine.addOperator(new RemoveComponent().setSelectorQuery("nodes[{name=*}]/hosts[{name=*}]/components[{name=*}]"));
-        engine.addOperator(new RemoveChildNode().setSelectorQuery("nodes[{ typeDefinition.name = *CustomerNode }]"));
-        //engine.addOperator(new MoveNode().setSelectorQuery("nodes[{ typeDefinition.name = *CustomerNode }]"));
+        RandomSolutionFactory solutionFactory = new RandomSolutionFactory();
+        solutionFactory.addType("ItemDB").addType("LoadBalancer").addType("PaymentDB").addType("UserDB").addType("WebFrontend");
 
-        /* Configure fitness */
-        engine.addFitnessFuntion(new ConsumptionFitness());
-        engine.addFitnessFuntion(new IsolationFitness());
-        engine.addFitnessFuntion(new RedondencyFitness().setAllTypes(operator.getAllTypes()));
-        engine.addFitnessFuntion(new MaximizeChildNodesFitness());
-        engine.addFitnessFuntion(new CompletenessFitness().setAllTypes(operator.getAllTypes()));
+        KevoreeCompositeFitnessFunction compositeFunction = new KevoreeCompositeFitnessFunction();
 
+        HashMap<String, KevoreeFitnessFunction> fintessNames = new HashMap<String, KevoreeFitnessFunction>();
+         /* Configure fitness */
+        ConsumptionFitness consumptionFitness = new ConsumptionFitness();
+        fintessNames.put(consumptionFitness.getName(), consumptionFitness);
+        compositeFunction.addFitness(consumptionFitness);
+        RedondencyFitness redondencyFitness = new RedondencyFitness().setAllTypes(solutionFactory.getAllTypes());
+        fintessNames.put(redondencyFitness.getName(), redondencyFitness);
+        compositeFunction.addFitness(redondencyFitness);
+        CompletenessFitness completenessFitness = new CompletenessFitness().setAllTypes(solutionFactory.getAllTypes());
+        fintessNames.put(completenessFitness.getName(), completenessFitness);
+        compositeFunction.addFitness(completenessFitness);
+        SecurityFitness securityFitness = new SecurityFitness();
+        fintessNames.put(securityFitness.getName(), securityFitness);
+        compositeFunction.addFitness(securityFitness);
+        fintessNames.put("mean", compositeFunction);
 
-        engine.setMaxGeneration(1000);
+        SolutionPloter ploter = new SolutionPloter();
+
+        List<ContainerRoot> result = new ArrayList<ContainerRoot>() ;
         long currentTime = System.currentTimeMillis();
-        List<KevoreeSolution> result = engine.solve();
-        System.out.println("Found "+result.size()+" solutions in " + (System.currentTimeMillis() - currentTime) + " ms");
-        SolutionFilter filter = new SolutionFilter();
-        for (KevoreeSolution solution : filter.order(filter.filterSolution(result))) {
-            solution.print(System.out);
+        for(int i = 0 ; i < 200 ; i++) {
+            ContainerRoot solution = solutionFactory.createRandomSolution();
+            result.add(solution);
         }
+
+        HashMap<String, LinkedList<Double>> timeResults = new HashMap<String, LinkedList<Double>>();
+        for(ContainerRoot model : result) {
+            for(String fitness : fintessNames.keySet()) {
+                LinkedList<Double> fitnessValues = timeResults.get(fitness);
+                if(fitnessValues == null) {
+                    fitnessValues = new LinkedList<Double>();
+                    timeResults.put(fitness, fitnessValues);
+                }
+                fitnessValues.addLast(fintessNames.get(fitness).evaluate(model));
+            }
+        }
+
+        ploter.plotResult(timeResults);
+
+        System.out.println("Found " + result.size() + " solutions in " + (System.currentTimeMillis() - currentTime) + " ms");
+
     }
 
 
--- org.kevoree.genetic.cloud.reasoner/src/main/java/org/kevoree/genetic/cloud/reasoner/population/RandomSolutionFactory.java
+++ org.kevoree.genetic.cloud.reasoner/src/main/java/org/kevoree/genetic/cloud/reasoner/population/RandomSolutionFactory.java
@@ -1,9 +1,13 @@
 package org.kevoree.genetic.cloud.reasoner.population;
 
 import org.kevoree.*;
+import org.kevoree.genetic.cloud.reasoner.operators.AddVirtualNodeOperator;
+import org.kevoree.genetic.framework.KevoreeMutationOperator;
 import org.kevoree.impl.DefaultKevoreeFactory;
 import org.kevoree.loader.ModelLoader;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -19,7 +23,27 @@ public class RandomSolutionFactory {
     private static Integer numberOfInfraNode_fullPower = 2;
     private Random rand = new Random();
 
-    public void createRandomSolution() {
+
+    private List<String> types = new ArrayList<String>();
+
+    public RandomSolutionFactory addType(String t) {
+        types.add(t);
+        return this;
+    }
+
+    public RandomSolutionFactory setAllTypes(List<String> _types) {
+        types = _types;
+        return this;
+    }
+
+    public List<String> getAllTypes(){
+        return types;
+    }
+
+
+
+
+    public ContainerRoot createRandomSolution() {
         ModelLoader loader = new ModelLoader();
         ContainerRoot rootModel = loader.loadModelFromStream(this.getClass().getResourceAsStream("/KEV-INF/lib.kev")).get(0);
         /* Fix Immutable */
@@ -34,19 +58,59 @@ public class RandomSolutionFactory {
         }
         //Init Infra
         //Fill Customer LowPowerNode
+
+        int vcpuMax = 0;
+
         for (int i = 0; i < numberOfInfraNode_lowPower; i++) {
             ContainerNode node = factory.createContainerNode();
             node.setName("ARMINode_" + i);
-            node.setTypeDefinition(rootModel.findTypeDefinitionsByID("ARMInfraNode"));
+            TypeDefinition td = rootModel.findTypeDefinitionsByID("ARMInfraNode");
+            for (DictionaryValue v : td.getDictionaryType().getDefaultValues()) {
+                if (v.getAttribute().getName().equals("vcpu")) {
+                    vcpuMax += Integer.parseInt(v.getValue());
+                }
+            }
+            node.setTypeDefinition(td);
             rootModel.addNodes(node);
         }
         //Fill Customer FullPowerNode
         for (int i = 0; i < numberOfInfraNode_fullPower; i++) {
             ContainerNode node = factory.createContainerNode();
             node.setName("XeonINode_" + i);
-            node.setTypeDefinition(rootModel.findTypeDefinitionsByID("XeonInfraNode"));
+            TypeDefinition td = rootModel.findTypeDefinitionsByID("XeonInfraNode");
+            for (DictionaryValue v : td.getDictionaryType().getDefaultValues()) {
+                if (v.getAttribute().getName().equals("vcpu")) {
+                    vcpuMax += Integer.parseInt(v.getValue());
+                }
+            }
+            node.setTypeDefinition(td);
             rootModel.addNodes(node);
         }
+
+        KevoreeMutationOperator addVirtualNodeOperator = new AddVirtualNodeOperator().setSelectorQuery("nodes[{ typeDefinition.name = *InfraNode }]");
+        int nbVNodes = rand.nextInt(vcpuMax);
+        for(int i = 0; i < nbVNodes ; i++) {
+            rootModel = addVirtualNodeOperator.mutate(rootModel);
+        }
+
+        List<Object> vNodes = rootModel.selectByQuery("nodes[{ typeDefinition.name = *CustomerNode }");
+        int redondency = 5;
+        for(String type : types) {
+            int typeRed = rand.nextInt(redondency);
+
+            for(int r = 0; r < typeRed; r++) {
+                ContainerNode node = (ContainerNode)vNodes.get(rand.nextInt(vNodes.size()));
+                TypeDefinition td = rootModel.findTypeDefinitionsByID(type);
+                if (td != null) {
+                    ComponentInstance inst = factory.createComponentInstance();
+                    inst.setName(type + "_" + rand.nextInt());
+                    inst.setTypeDefinition(td);
+                    node.addComponents(inst);
+                }
+            }
+        }
+
+        return rootModel;
 
     }
 
